642a7641567f2f2308aa7a94612f96da46a16fbf
Juan Diego Gramajo <juan.gramajo@xoom.com>
2016-02-18 16:30:05 -0600

    Introduces bugs for candidate tests.

--- XoomPayPal.py
+++ XoomPayPal.py
@@ -1,15 +1,13 @@
-import sys
-
 def eval_number(number = 25):
     s = ''
-    if number % 3 == 0:
+    if number % 3 = 0:
         s += 'Xoom' 
     if number % 5 == 0:
         s += 'PayPal'
-    if s == '':
+    if s == ''
         return number
     else:
         return s
 
-for num in range(1, int(sys.argv[1]) + 1):
+for num in range(1, int(sys.argv[0]) + 1):
     print(eval_number(num))
